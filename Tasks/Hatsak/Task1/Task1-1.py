b8593064a583d00b38068e0740bd65fae579819a
hatsakandrey <hatsakandrey@gmail.com>
2023-07-04 12:45:01 +0300

    Done first task_2

--- Tasks/Hatsak/Task1/Task1-1.py
+++ Tasks/Hatsak/Task1/Task1-1.py
@@ -1,3 +1,25 @@
-cel = input('INPUT\n')
-far = 1.8 * float(cel) + 32
-print(far)
+def askToExit():
+    print("Do you want to exit program?")
+    print("Enter 'Y' to say 'yes', 'N' to say 'no'")
+    answer = input()
+    if answer.upper() == 'N':
+        return True
+    elif answer.upper() == 'Y':
+        return False
+    else:
+        return askToExit()
+
+
+print("Hello! This program converts Celsius to Fahrenheit")
+flag = True
+
+while flag:
+    cel = input("Enter value in Celsius\n")
+    far = 1.8 * float(cel) + 32
+    print(far)
+    flag = askToExit()
+
+
+
+
+
