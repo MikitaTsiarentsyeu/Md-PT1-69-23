78468eff2065331d8539382c0728a272b3918805
hatsakandrey <hatsakandrey@gmail.com>
2023-07-04 17:43:25 +0300

    update task1-2 float checker

--- Tasks/Hatsak/Task1/Task1-2.py
+++ Tasks/Hatsak/Task1/Task1-2.py
@@ -6,8 +6,7 @@ radius = input()
 
 if radius.isdigit():
     print("The area of your circle is: ", round(int(radius)**2 * math.pi, 2))
-elif '.' in radius[1:-1] and radius.count('.') == 1 \
-        and (radius[0:radius.index('.')] + radius[radius.index('.')+1:]).isdigit():
+elif '.' in radius[1:-1] and (radius[0:radius.index('.')] + radius[radius.index('.')+1:]).isdigit():
     print("The area of your circle is: ", round(float(radius)**2 * math.pi, 2))
 else:
     print("Please, enter only integer or float values."
