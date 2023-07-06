b8cff430d156792c81c12298348486afe127d362
hatsakandrey <hatsakandrey@gmail.com>
2023-07-06 14:21:18 +0300

    done 1-3

--- Tasks/Hatsak/Task1/Task1-3.py
+++ Tasks/Hatsak/Task1/Task1-3.py
@@ -2,6 +2,18 @@
 
 print("Hello! This program will convert your speed from kilometers per hour to meters per second")
 print("Enter your speed:", end=' ')
-speed = input()
-answer = int(speed)*1000/3600
+
+
+def enter():
+    speed = input()
+    try:
+        speed = float(speed)
+        return speed
+    except:
+        print("Invalid input! Try again!")
+        return enter()
+
+
+speed = enter()
+answer = round(int(speed)*1000/3600, 2)
 print(f"{speed} km/h = {answer} m/s")
