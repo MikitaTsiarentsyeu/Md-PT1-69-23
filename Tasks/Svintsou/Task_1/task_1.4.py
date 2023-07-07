256275ca8f90bf3c3a984a5486eb409ccb54594b
iskatelxd <61771901+iskatelxd@users.noreply.github.com>
2023-07-07 13:26:21 +0300

    init repository

--- Tasks/Svintsou/Task_1/task_1.4.py
+++ Tasks/Svintsou/Task_1/task_1.4.py
@@ -1,10 +1,10 @@
-"""usd_to_byn = 3  # соотношение USD к BYN
+usd_to_byn = 3  # соотношение USD к BYN
 
 usd_amount = float(input("Введите сумму в USD: "))
 
 byn_amount = usd_amount * usd_to_byn
 
-print(f"Сумма в BYN: {byn_amount:.2f}")"""
+print(f"Сумма в BYN: {byn_amount:.2f}")
 
 
 
