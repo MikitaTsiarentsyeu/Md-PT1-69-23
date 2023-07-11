f052ae478c9bc5a4b14302578445251410bd7a9b
ROANVL <romanychev@tut.by>
2023-07-11 20:22:03 +0300

    Create test.py

---
+++ Tasks/Romanychev/Task2/test.py
@@ -0,0 +1,29 @@
+import user_interface as ui
+from time_formatting import cur_time
+from validation import validate_time_format
+from itertools import product
+
+
+def main():
+    MAX_ATTEMPTS = 3
+    attempt_count = 0
+
+    res = [f"{h:02d}:{m:02d}" for h, m in product(range(24), range(0, 60, 1))]
+
+    for i in res[:770]:
+        while attempt_count < MAX_ATTEMPTS:
+
+            time_input = i
+
+            if validate_time_format(time_input):
+                formatted_time = cur_time(time_input)
+
+            ui.print_formatted_time(formatted_time)
+
+            break
+
+    ui.print_footer()
+
+
+if __name__ == "__main__":
+    main()
