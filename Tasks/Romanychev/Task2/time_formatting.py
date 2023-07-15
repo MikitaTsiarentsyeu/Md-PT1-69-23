d958b36a1272e6da3b0cd82b67a30ea2c18e0ed6
ROANVL <romanychev@tut.by>
2023-07-15 18:13:46 +0300

    Update dictionary

--- Tasks/Romanychev/Task2/time_formatting.py
+++ Tasks/Romanychev/Task2/time_formatting.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from dictionaries import dict_min, dict_hour
+from dictionaries import dict
 
 
 def convert_time_to_spoken_word(time_input=None):
@@ -18,23 +18,30 @@ def convert_time_to_spoken_word(time_input=None):
     """
     if time_input is not None:
         hour, minute = map(int, time_input.split(':'))
+        h1, h2, m1, m2 = [int(h) for i in time_input.split(':') for h in i]
         current_time_str = time_input
     else:
         now = datetime.now()
         hour = now.hour
         minute = now.minute
         current_time_str = now.strftime('%H:%M')
+        h1, h2, m1, m2 = [int(h)
+                          for i in current_time_str.split(':') for h in i]
 
     match (minute):
         case 0:
-            return f"{current_time_str} - {dict_hour[hour % 12][0]} {dict_hour[hour % 12][2]} ровно"
-        case m if m < 30:
-            return f"{current_time_str} - {dict_min[minute][0]} {dict_min[minute][2]} {dict_hour[(hour + 1) % 12][1]}"
+            return f"{current_time_str} - {dict[hour % 12][5]} {dict[hour % 12][6]} ровно"
+        case m if m <= 20:
+            return f"{current_time_str} - {dict[minute][0]} {dict[minute][1]} {dict[(hour + 1) % 12][2]}"
+        case m if 21 <= m <= 29:
+            return f"{current_time_str} - {dict[20][0]} {dict[m2][0]} {dict[m2][1]} {dict[(hour + 1) % 12][2]}"
         case 30:
-            return f"{current_time_str} - половина {dict_hour[hour % 12 + 1][1]}"
-        case m if 30 < m < 45:
-            return f"{current_time_str} - {dict_min[minute][0]} {dict_min[minute][2]} {dict_hour[(hour + 1) % 12][1]}"
+            return f"{current_time_str} - половина {dict[(hour + 1) % 12][2]}"
+        case m if 31 <= m <= 39:
+            return f"{current_time_str} - {dict[30][0]} {dict[m2][0]} {dict[m2][1]} {dict[(hour + 1) % 12][2]}"
+        case m if 40 <= m <= 44:
+            return f"{current_time_str} - {dict[40][0]} {dict[m2][0]} {dict[m2][1]} {dict[(hour + 1) % 12][2]}"
         case m if m >= 45:
-            return f"{current_time_str} - без {dict_min[60 - minute][1]} {dict_min[60 - minute][3]} {dict_hour[(hour + 1) % 12][0]}"
+            return f"{current_time_str} - без {dict[60 - minute][3]} {dict[m2][1]} {dict[(hour + 1) % 12][5]}"
         case _:
-            return ""
+            return ''
