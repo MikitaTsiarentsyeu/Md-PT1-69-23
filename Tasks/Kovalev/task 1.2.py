640c795057b50717ef3f9b32b95f24a8b3b30a75
folenz <kovalencn6@gmail.com>
2023-07-18 14:52:12 +0300

    don task 1.1

---
+++ Tasks/Kovalev/task 1.2.py
@@ -0,0 +1,8 @@
+#Hi this is a programm to calculate the area of a circle through the radius
+import math
+radius = input("Enter only positive values")
+square = math.pi*float(radius)**2
+print(square)
+exit
+
+
