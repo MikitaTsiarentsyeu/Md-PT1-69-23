0f9b8b3fdb8c2fac76c6510bc7a05c9ec09beb50
sherel <31521481+6ober@users.noreply.github.com>
2023-07-19 19:47:12 +0300

    add Task3_5.py

---
+++ Tasks/Sherel/Task3/Task3_5.py
@@ -0,0 +1,13 @@
+"""5. Write a program that takes a list of strings as input and returns a list with all strings that have a length greater than 5 characters."""
+
+user_input = input("Enter your string:\n")
+user_input = user_input.replace(',', '').replace('.', '').replace(':', '').replace('!', '').replace('?', '').replace('-', '')
+user_input = user_input.split()
+
+myList = []
+for i in user_input:
+    length = len(i)
+    if int(length) > 5:
+        myList.append(i)
+
+print("list with all strings that have a length greater than 5 characters:\n", myList)
