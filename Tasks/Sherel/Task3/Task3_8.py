d045d28e13ab0556a8e9dd992c891dbcbd3a81a2
sherel <31521481+6ober@users.noreply.github.com>
2023-07-19 20:31:57 +0300

    fix

--- Tasks/Sherel/Task3/Task3_8.py
+++ Tasks/Sherel/Task3/Task3_8.py
@@ -1,4 +1,4 @@
-"""2. Write a program that takes a list of numbers as input and returns the sum of all even numbers in the list."""
+"""8. Write a program that takes a list of numbers as input and returns the average of all numbers in the list."""
 
 user_input = input("Enter your list of numbers separated by a space:\n")
 user_input = user_input.split()
