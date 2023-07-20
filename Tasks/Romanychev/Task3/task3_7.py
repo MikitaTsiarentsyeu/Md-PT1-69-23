4b261a24c7022475f2a8e06e8e9854c5d7802568
ROANVL <romanychev@tut.by>
2023-07-20 13:05:05 +0300

    task3_7 Refactor code and add documentation
    The code has been refactored with improved variable names for better readability. Additionally, user-friendly documentation has been added at the beginning of the script to explain its purpose and usage.
    
    The script allows users to input a string of words, and it will swap the case of letters in each word and display the result. The script provides a simple text-based interface for a smooth user experience.

--- Tasks/Romanychev/Task3/task3_7.py
+++ Tasks/Romanychev/Task3/task3_7.py
@@ -1,35 +1,73 @@
-def is_valid_input(input_str):
+"""
+This script allows the user to enter a string of words, swaps the case of the letters in the words,
+and then displays the resulting swapped string. The script provides a simple text-based interface
+to interact with the user.
+
+Usage:
+1. Run the script.
+2. Enter a string containing words only when prompted.
+3. The script will swap the case of letters in each word of the input string.
+4. The swapped string will be displayed.
+5. The script will then ask if you want to continue.
+6. If you enter 'yes', the process will repeat from step 2.
+7. If you enter 'no', the script will terminate.
+8. The script handles invalid inputs and keyboard interrupts gracefully.
+
+Note:
+- The script considers a word to be a sequence of alphabetic characters without spaces.
+- If you enter a string with digits, special characters, or spaces within words,
+  it will be considered invalid, and you will be asked to enter a new string.
+
+Enjoy swapping the case of your words with this simple script!
+"""
+
+
+def is_valid_word_input(input_str):
     words = input_str.split()
     return all(word.isalpha() for word in words)
 
 
-def get_valid_input():
+def get_valid_word_input():
     while True:
-        user_input = input("Enter a string: ")
-        if is_valid_input(user_input):
-            return user_input
-        else:
-            print("Invalid input. Please enter only words.")
+        try:
+            user_input = input("Enter a string of words: ")
+            if is_valid_word_input(user_input):
+                return user_input
+            else:
+                print("Invalid input. Please enter only words.")
+        except KeyboardInterrupt:
+            print("\nProgram terminated by user.")
+            exit(0)
 
 
 def swap_case(input_str):
     return input_str.swapcase()
 
 
+def get_continue_choice():
+    while True:
+        try:
+            choice = input("Do you want to continue? (yes/no): ").lower()
+            if choice == "yes":
+                return True
+            elif choice == "no":
+                return False
+            else:
+                raise ValueError
+        except KeyboardInterrupt:
+            print("\nProgram terminated by user.")
+            exit(0)
+        except ValueError:
+            print("Please enter 'yes' or 'no.'")
+
+
 def main():
     while True:
-        user_input = get_valid_input()
-        swapped_str = swap_case(user_input)
-        print("Swapped string:", swapped_str)
+        user_input = get_valid_word_input()
+        swapped_string = swap_case(user_input)
+        print("Swapped string:", swapped_string)
 
-        while True:
-            choice = input("Do you want to continue? (yes/no): ").lower()
-            if choice in ["yes", "no"]:
-                break
-            else:
-                print("Please enter 'yes' or 'no'.")
-
-        if choice == "no":
+        if not get_continue_choice():
             break
 
 
