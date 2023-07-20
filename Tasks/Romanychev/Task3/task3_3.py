9fe58c39a48b41f9d78d2ee1c4f03c8505437889
ROANVL <romanychev@tut.by>
2023-07-20 11:34:50 +0300

    task3_3 Refactor Vowel Counter and Language Detection script with improved input handling and execution time measurement
        Refactored the code to improve readability and maintainability.
        Added input validation for source choice and language selection.
        Implemented a function decorator to measure the execution time of functions.
        Updated the get_user_input() function to handle file input and language detection more effectively.
        Modified the plot_histogram() function to display a message if no vowels are found in the input string.
        Reorganized the main program loop to handle user input and processing more efficiently.
        Added a prompt to ask the user if they want to run the program again or exit after each run.
    
    Overall, the script now provides a more user-friendly experience with improved input validation and better error handling. Additionally, the code has been optimized to calculate vowel and word counts efficiently, and it displays the histogram of vowel counts based on the detected language. The script also presents word counts, including the 10 most frequent words and the total number of unique words in the input string.

--- Tasks/Romanychev/Task3/task3_3.py
+++ Tasks/Romanychev/Task3/task3_3.py
@@ -1,20 +1,55 @@
+"""
+Vowel and Word Counter with Language Detection Script
+This script allows you to input a text string either from the keyboard or a file and detect its language.
+It then counts the number of vowels and words in the input string based on the detected language and plots a histogram
+to visualize the distribution of vowel counts across supported languages.
+Additionally, it displays the word counts for the input string along with the 10 most frequent words, and the total number
+of unique words in the input.
+
+Usage:
+1. Choose the source of the input string:
+    - If you choose '1', you can input a string from the keyboard.
+    - If you choose '2', the script will attempt to read a text file ('test(war and peace)_task3_1.txt')
+      and detect its language.
+
+2. If you input the string from the keyboard:
+    - You will be prompted to choose the language of the input string from the following options:
+        'en' - English,
+        'ru' - Russian,
+        'pl' - Polish,
+        'de' - German,
+        'fr' - French.
+
+3. The script will detect the language of the input string and count the number of vowels and words in it based on the chosen language.
+
+4. A histogram will be plotted, showing the distribution of vowel counts for the selected language.
+
+Supported Languages and Vowels:
+- English: ['a', 'e', 'i', 'o', 'u']
+- Russian: ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+- Polish: ['a', 'ą', 'e', 'ę', 'i', 'o', 'ó', 'u', 'y']
+- German: ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü']
+- French: ['a', 'e', 'i', 'o', 'u']
+
+Note: The script uses the 'langdetect' library for language detection. Please make sure to install it using 'pip install langdetect'
+before running the script.
+
+Author: ROANVL
+"""
+
 import time
 import re
-import string
 import matplotlib.pyplot as plt
 from langdetect import detect_langs
 from collections import Counter
 
+
 # Function to detect the language of a given text
-
-
 def detect_language(text):
     """
     Detects the language of the given text.
-
     Args:
         text (str): The input text to detect the language.
-
     Returns:
         tuple: A tuple containing the detected language code and a dictionary of
             language probabilities. The language code represents the detected
@@ -30,29 +65,72 @@ def detect_language(text):
 
 
 # Function to choose the source of the input string (keyboard or file)
+VALID_SOURCE_CHOICES = ['1', '2']
+
+
 def choose_source():
-    source_choice = None
-    while source_choice not in ['1', '2']:
+    while True:
         source_choice = input(
             "Choose the source of the string:\n1 - keyboard input,\n2 - file input.\nYour choice: ")
-        if source_choice not in ['1', '2']:
+        if source_choice in VALID_SOURCE_CHOICES:
+            return source_choice
+        else:
             print("Invalid choice. Please select 1 or 2.")
-    return source_choice
 
 
 # Function to choose the language of the input string
+VALID_LANGUAGE_CHOICES = ['en', 'ru', 'pl', 'de', 'fr']
+
+
 def choose_language():
-    language = None
-    while language not in ['en', 'ru', 'pl', 'de', 'fr']:
+    while True:
         language = input(
-            "Choose the language of the input string:\n'en' - English,\n'ru' - Russian,\n"
-            "'pl' - Polish,\n'de' - German,\n'fr' - French.\nYour choice: ")
-        if language not in ['en', 'ru', 'pl', 'de', 'fr']:
+            f"Choose the language of the input string:\nValid choices: {', '.join(VALID_LANGUAGE_CHOICES)}\nYour choice: ")
+        if language in VALID_LANGUAGE_CHOICES:
+            return language
+        else:
             print("Invalid choice. Please select from the provided languages.")
-    return language
+
+
+# Function to get the user input based on the source choice
+def get_user_input(source_choice):
+    if source_choice == "1":
+        while True:
+            language = choose_language()
+            user_input = input("Enter a string: ")
+            if language and user_input:  # Check if both inputs are valid
+                break
+            print("Invalid input. Please try again.")
+    else:
+        try:
+            with open('test(war and peace)_task3_1.txt', 'r') as file:
+                user_input = file.read().strip()
+            detected_language = detect_language(user_input)
+            language = detected_language[0]
+        except FileNotFoundError:
+            print("File not found.")
+            return None, None
+        except Exception as e:
+            print(f"An error occurred while reading the file: {e}")
+            return None, None
+    return language, user_input
+
+
+def measure_execution_time(func):
+    # Function decorator to measure the execution time of a function
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(
+            f"Execution time for '{func.__name__}': {execution_time:.5f} seconds")
+        return result
+    return wrapper
 
 
 # Function to count the number of vowels in a string
+@measure_execution_time
 def count_vowels(input_string, vowels):
     """
     Counts the number of vowels in a given string.
@@ -64,15 +142,13 @@ def count_vowels(input_string, vowels):
     Returns:
         dict: A dictionary containing the vowel counts, where the keys are the vowels and the values are the counts.
     """
-    start_time = time.time()
     input_string_lower = input_string.lower()
     vowel_counts = {vowel: input_string_lower.count(vowel) for vowel in vowels}
-    end_time = time.time()
-    execution_time = end_time - start_time
-    return vowel_counts, execution_time
+    return vowel_counts
 
 
 # Function to count the occurrences of each word in a string
+@measure_execution_time
 def count_words(input_string):
     """
     Counts the occurrences of each word in a given string, excluding punctuation and special characters.
@@ -93,32 +169,35 @@ def count_words(input_string):
 def plot_histogram(vowel_counts_by_language):
     """
     Plots a histogram showing the distribution of vowel counts by language.
-
     Args:
         vowel_counts_by_language (dict): A dictionary containing the vowel counts for each language.
             The keys represent the language, and the values are dictionaries with vowel counts,
             where the keys are the vowels and the values are the counts.
-
     Returns:
         None
     """
     fig, ax = plt.subplots()
 
-    for i, (language, vowel_counts) in enumerate(vowel_counts_by_language.items()):
-        x = range(len(vowel_counts))
-        height = list(vowel_counts.values())
+    for index, (language, vowel_counts) in enumerate(vowel_counts_by_language.items()):
+        x_values = range(len(vowel_counts))
+        vowel_counts_list = list(vowel_counts.values())
         labels = list(vowel_counts.keys())
 
-        ax.bar(x, height, align='center', label=language, color=plt.cm.Set3(i))
+        ax.bar(x_values, vowel_counts_list, align='center',
+               label=language, color=plt.cm.Set3(index))
 
-        for j, (h, l) in enumerate(zip(height, labels)):
-            ax.text(j, h + 0.02 * max(height), str(h), ha='center')
-            percentage = (h / sum(height)) * 100
-            ax.text(j, h / 2, f"{percentage:.1f}%", ha='center', color='white')
+        for bar_index, (height, label) in enumerate(zip(vowel_counts_list, labels)):
+            ax.text(bar_index, height + 0.02 *
+                    max(vowel_counts_list), str(height), ha='center')
+
+            if vowel_counts_list and sum(vowel_counts_list) != 0:
+                percentage = (height / sum(vowel_counts_list)) * 100
+                ax.text(bar_index, height / 2,
+                        f"{percentage:.1f}%", ha='center', color='white')
 
     ax.set_xlabel('Vowel Characters')
     ax.set_ylabel('Count of Vowels')
-    total_vowels = sum(vowel_counts.values())
+    total_vowels = sum(vowel_counts_list)
     ax.set_title(
         f"COUNT OF VOWELS\n(text language - {''.join(language)})\ntotal number of vowels: {total_vowels}")
 
@@ -129,51 +208,36 @@ def plot_histogram(vowel_counts_by_language):
 
     plt.tight_layout()
     ax.legend()
-    plt.xticks(x, labels)
-    plt.show()
+    plt.xticks(x_values, labels)
+
+    if not any(vowel_counts_list) or sum(vowel_counts_list) == 0:
+        print("No vowels found in the input string.")
+    else:
+        plt.show()
 
 
 # Main program loop
 while True:
-    start_time = time.time()  # Start time of the program
 
     # Choose the source of the input string (keyboard or file)
     source_choice = choose_source()
+    language, user_input = get_user_input(source_choice)
 
-    if source_choice == "1":
-        # If keyboard input is chosen, prompt the user to choose the language and enter the string
-        language = choose_language()
-        user_input = input("Enter a string: ")
-    else:
-        try:
-            # If file input is chosen, read the string from a file and detect its language
-
-            # The language in the filename 'test(en)_task3_1.txt' can be changed to any language from the following list:
-            # ['en', 'ru', 'pl', 'de', 'fr']
-
-            with open('test(war and peace)_task3_1.txt', 'r') as file:
-                user_input = file.read().strip()
-            detected_language = detect_language(user_input)
-            language = detected_language[0]
-        except FileNotFoundError:
-            print("File not found.")
-        except Exception as e:
-            print("An error occurred while reading the file:", str(e))
-
-    # Define the vowels for each supported language
-    vowels_by_language = {
-        'en': ['a', 'e', 'i', 'o', 'u'],
-        'ru': ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'],
-        'pl': ['a', 'ą', 'e', 'ę', 'i', 'o', 'ó', 'u', 'y'],
-        'de': ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü'],
-        'fr': ['a', 'e', 'i', 'o', 'u']
-    }
+    if language is not None and user_input is not None:
+        # Define the vowels for each supported language
+        vowels_by_language = {
+            'en': ['a', 'e', 'i', 'o', 'u'],
+            'ru': ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'],
+            'pl': ['a', 'ą', 'e', 'ę', 'i', 'o', 'ó', 'u', 'y'],
+            'de': ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü'],
+            'fr': ['a', 'e', 'i', 'o', 'u']
+        }
 
     # Count the number of vowels and words in the input string for the selected language
     vowel_counts_by_language = {}
     word_counts_by_language = {}
 
-    vowel_counts, execution_time = count_vowels(
+    vowel_counts = count_vowels(
         user_input, vowels_by_language[language])
 
     word_counts = count_words(user_input)
@@ -182,11 +246,8 @@ while True:
 
     word_counts_by_language[language] = word_counts
 
-    # Display the execution time for the specific function
-    print(f"Execution time for 'count_vowels': {execution_time:.5f} seconds")
-
     # Plot a histogram of vowel counts by language
-    # (the same as before, no changes needed)
+    plot_histogram(vowel_counts_by_language)
 
     # Display the word counts for the input string
     print("\nWord counts for the input string:")
@@ -199,16 +260,17 @@ while True:
     # Display the total number of unique words
     print(f"\nTotal number of unique words: {unique_word_count}")
 
-    # Display the total execution time
-    end_time = time.time()  # End time of the program
-    total_execution_time = end_time - start_time  # Total execution time
-    print(f"\nTotal execution time: {total_execution_time:.5f} seconds")
+    # Prompt the user if they want to run the program again or exit
+    while True:
+        restart_choice = input(
+            "Do you want to run the program again? (y/n): ").lower()
+        if restart_choice in ['y', 'n']:
+            break
+        else:
+            print("Invalid choice. Please enter 'y' or 'n'.")
 
-    # Prompt the user if they want to run the program again
-    restart_choice = input("Do you want to run the program again? (y/n): ")
-    while restart_choice.lower() not in ['y', 'n']:
-        print("Invalid choice. Please enter 'y' or 'n'.")
-        restart_choice = input("Do you want to run the program again? (y/n): ")
-
-    if restart_choice.lower() == 'n':
+    if restart_choice == 'n':
         break  # Exit the loop if the user doesn't want to restart
+
+# End of the program
+print("Thank you for using the Vowel Counter and Language Detection script. Goodbye!")
