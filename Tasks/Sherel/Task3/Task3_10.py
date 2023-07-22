dfc419c94423176bf578208a25018c1664d736d3
sherel <31521481+6ober@users.noreply.github.com>
2023-07-22 13:23:13 +0300

    add second option check max number

--- Tasks/Sherel/Task3/Task3_10.py
+++ Tasks/Sherel/Task3/Task3_10.py
@@ -4,23 +4,26 @@ user_input = input("Enter your list of numbers separated by a space:\n")
 user_input = user_input.split()
 
 #check input the largest prime number and creat the list.
-miList = []
-for num in user_input:
+myList = []
+for x in user_input:
     rubilnik = True
-    if int(num) <= 1:
+    if int(x) <= 1:
         continue
-    for i in range(2, int(int(num) ** 0.5) + 1):
-        if int(num) % i == 0:
+    for i in range(2, int(int(x) ** 0.5) + 1):
+        if int(x) % i == 0:
             rubilnik = False
             break
     if rubilnik is True:
-        miList.append(num)
-#print(miList)
+        myList.append(x)
+#print(myList)
 
-#check largest int in creat list 'mtList'
-max_number_p = 0
-for i in miList:
-    if int(i) > int(max_number_p):
-        max_number_p = i
+#check max number in list option 1
+max_number_p = max(myList, key=int)
+
+##check max number in list option 2
+# max_number_p = 0
+# for i in myList:
+#     if int(i) > int(max_number_p):
+#         max_number_p = i
 
 print(max_number_p)
