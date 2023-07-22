7b469084bb4830eec834229f74ee590ce7ff3cf8
sherel <31521481+6ober@users.noreply.github.com>
2023-07-22 13:41:37 +0300

    optimization

--- Tasks/Sherel/Task3/Task3_3.py
+++ Tasks/Sherel/Task3/Task3_3.py
@@ -1,7 +1,8 @@
 """3. Write a program that takes a string as input and returns a dictionary with the count of each word in the string."""
 
 user_input = input("Enter your string:\n")
-user_input = user_input.replace(',', '').replace('.', '').replace(':', '').replace('!', '').replace('?', '').replace('-', '')
+rep = ",.:;!?-#@$%^&*()_+|/"
+user_input = ''.join([x for x in user_input if x not in rep]) #add comprehensions from lessons 22.07
 user_input = user_input.split()
 
 myList = {}
