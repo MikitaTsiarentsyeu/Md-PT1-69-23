cdc74dd7119362c149f6bd1e0f947e0e183300de
szerel <artyom_sherel@mail.ru>
2023-08-02 16:16:46 +0300

    fix

--- Tasks/Sherel/Task5/Task5_1-5.py
+++ Tasks/Sherel/Task5/Task5_1-5.py
@@ -62,7 +62,8 @@ print(f"""Count lower case symbols: {lower_count}\nCount upper case symbols: {up
 # Count upper case symbols: 3
 
 
-# 5. Write a function that takes an ordered list of numbers without duplicates and returns a string with ranges for those numbers, check the example below:
+# 5. Write a function that takes an ordered list of numbers without duplicates
+# and returns a string with ranges for those numbers, check the example below:
 # get_ranges([0, 1, 2, 3, 4, 7, 8, 10])  ->  "0-4, 7-8, 10"
 # get_ranges([4,7,10])  -> "4, 7, 10"
 # get_ranges([2, 3, 8, 9])  -> "2-3, 8-9"
