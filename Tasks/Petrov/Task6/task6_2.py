e0f6c2638fbd63f9b2ca04b60ba67292f5e9bfd4
KortezR <wowikus99@mail.ru>
2023-08-07 11:52:45 +0300

    Do task6_2

---
+++ Tasks/Petrov/Task6/task6_2.py
@@ -0,0 +1,19 @@
+from string import punctuation
+
+
+def ispalindrome(text):
+    if len(text) <= 1:
+        return True
+    else:
+        if text[0] == text[-1]:
+            return ispalindrome(text[1:-1])
+        return False
+
+
+line = input("Input text: ")
+without_all = str.maketrans("", "", punctuation + " ")
+flag_without_all = ispalindrome(line.lower().translate(without_all))
+with_spaces = str.maketrans("", "", punctuation)
+flag_with_spaces = ispalindrome(line.lower().translate(with_spaces))
+print(f"Is your input a palindrome? {flag_without_all}")
+print(f"Is your input a palindrome with spaces? {flag_with_spaces}")
