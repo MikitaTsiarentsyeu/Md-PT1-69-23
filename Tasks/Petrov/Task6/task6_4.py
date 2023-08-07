95b0ba4eeed5787b310251bec2f1b3445685a40c
KortezR <wowikus99@mail.ru>
2023-08-07 18:32:59 +0300

    Do task6_4

---
+++ Tasks/Petrov/Task6/task6_4.py
@@ -0,0 +1,30 @@
+from decimal import Decimal, InvalidOperation
+
+
+def power(number, pow_count):
+    if pow_count == 1:
+        return number
+    elif pow_count:
+        return number * power(number, pow_count-1)
+    else:
+        return 1
+
+
+while True:
+    try:
+        num = Decimal(input("Input a number: "))
+        break
+    except InvalidOperation:
+        print("This input should be a number")
+        continue
+
+while True:
+    try:
+        pow = int(input("Input a power: "))
+        break
+    except ValueError:
+        print("This input should be an integer number")
+        continue
+
+result = 1/power(num, -pow) if pow < 0 else power(num, pow)
+print(f"{num} in power {pow} is {result}")
