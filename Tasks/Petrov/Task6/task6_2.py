79167f02de129f33c74089769e4a7467a7e469bd
KortezR <wowikus99@mail.ru>
2023-08-08 14:30:23 +0300

    Editing task6_2 for input without spaces

--- Tasks/Petrov/Task6/task6_2.py
+++ Tasks/Petrov/Task6/task6_2.py
@@ -14,9 +14,10 @@ line = input("Input text: ")
 if line:
     without_all = str.maketrans("", "", punctuation + " ")
     flag_without_all = ispalindrome(line.lower().translate(without_all))
-    with_spaces = str.maketrans("", "", punctuation)
-    flag_with_spaces = ispalindrome(line.lower().translate(with_spaces))
     print(f"Is your input a palindrome? {flag_without_all}")
-    print(f"Is your input a palindrome with spaces? {flag_with_spaces}")
+    if " " in line:
+        with_spaces = str.maketrans("", "", punctuation)
+        flag_with_spaces = ispalindrome(line.lower().translate(with_spaces))
+        print(f"Is your input a palindrome with spaces? {flag_with_spaces}")
 else:
     print("There is no input")
