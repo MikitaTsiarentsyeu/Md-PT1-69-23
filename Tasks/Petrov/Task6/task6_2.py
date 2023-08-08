2d21346d44ac9149c6ac94f005cf7ea6585470b3
KortezR <wowikus99@mail.ru>
2023-08-08 22:04:38 +0300

    Edit task6_2 for better understanding

--- Tasks/Petrov/Task6/task6_2.py
+++ Tasks/Petrov/Task6/task6_2.py
@@ -10,14 +10,20 @@ def ispalindrome(text):
         return False
 
 
-line = input("Input text: ")
+line = input("Input text: ").lower()
 if line:
-    without_all = str.maketrans("", "", punctuation + " ")
-    flag_without_all = ispalindrome(line.lower().translate(without_all))
-    print(f"Is your input a palindrome? {flag_without_all}")
-    if " " in line:
-        with_spaces = str.maketrans("", "", punctuation)
-        flag_with_spaces = ispalindrome(line.lower().translate(with_spaces))
-        print(f"Is your input a palindrome with spaces? {flag_with_spaces}")
+    line = line.translate(str.maketrans("", "", punctuation))
+    flag_with_spaces = ispalindrome(line)
+    line = line.translate(str.maketrans("", "", " "))
+    flag_without_all = ispalindrome(line) if line else False
+    print("Is your input a palindrome? ", end="")
+    if flag_with_spaces and flag_without_all:
+        print("Yes")
+    elif flag_without_all:
+        print("Only without spaces in count")
+    elif flag_with_spaces:
+        print("Only with spaces in count")
+    else:
+        print("No")
 else:
     print("There is no input")
