706dd881b2791fc7ba90e5cb31f451356d60a65d
szerel <artyom_sherel@mail.ru>
2023-08-08 17:51:28 +0300

    add t6_3

--- Tasks/Sherel/Task6/Task6_1-5.py
+++ Tasks/Sherel/Task6/Task6_1-5.py
@@ -30,9 +30,30 @@ def check_str_palindrome(string):
         return check_str_palindrome(string[1:-1])
     return False
 
-input_str = input("Please enter your string for check a given string is a palindrome: ")
+# input_str = input("Please enter your string: ")
+# rep = ",.:;!?-#@$%^&*()_+|/'’"
+# input_str = ''.join([x for x in input_str if x not in rep])
+#
+# if check_str_palindrome(input_str.lower().replace(" ", '')):
+#     print("Yes, this string is a palindrome")
+# else:
+#     print("I'm sorry, but this string dont is a palindrome")
 
-if check_str_palindrome(input_str.lower().replace(" ", '')):
-    print("Yes, this string is a palindrome")
-else:
-    print("I'm sorry, but this string dont is a palindrome")
+"""3. Write a recursive function to count the number of occurrences of a given character in a string."""
+def count_occurrences(user_string, user_char):
+    if len(user_string) == 0:
+        return 0
+    elif user_string[0] == user_char:
+        return 1 + count_occurrences(user_string[1:], user_char)
+    else:
+        return count_occurrences(user_string[1:], user_char)
+
+# user_string = input("Please enter your string: ")
+# user_char = input("Please enter your char: ")
+#
+# print(count_occurrences(user_string, user_char))
+# Input 1: You can cage a swallow, can’t you, but you can’t swallow a cage, can you?
+# Input 2: y
+# Output: 3
+
+"""4. Write a recursive function to calculate the power of a given number."""
