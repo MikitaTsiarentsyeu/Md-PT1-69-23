a807b0aa9b68f82ec37872bb5d207304cd8c618d
KortezR <wowikus99@mail.ru>
2023-08-09 10:38:26 +0300

    Update task6_2.py

--- Tasks/Petrov/Task6/task6_2.py
+++ Tasks/Petrov/Task6/task6_2.py
@@ -13,7 +13,7 @@ def ispalindrome(text):
 line = input("Input text: ").lower()
 if line:
     line = line.translate(str.maketrans("", "", punctuation))
-    flag_with_spaces = ispalindrome(line)
+    flag_with_spaces = ispalindrome(line) if line else False
     line = line.translate(str.maketrans("", "", " "))
     flag_without_all = ispalindrome(line) if line else False
     print("Is your input a palindrome? ", end="")
