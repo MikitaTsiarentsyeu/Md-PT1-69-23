e15f43f7b6d2a91502b8e2f0de8eba2bcea6ca5b
szerel <artyom_sherel@mail.ru>
2023-08-09 17:16:08 +0300

    bug fix t6_5

--- Tasks/Sherel/Task6/Task6_1-5.py
+++ Tasks/Sherel/Task6/Task6_1-5.py
@@ -1,4 +1,4 @@
-szerel = {
+dictSzerel = {
     "work1": "1. Write a recursive function to reverse a string.",
     "work2": "2. Write a recursive function to check whether a given string is a palindrome.",
     "work3": "3. Write a recursive function to count the number of occurrences of a given character in a string.",
@@ -17,7 +17,7 @@ def reverse_string(string):
         return string
     return reverse_string(string[1:]) + string[0]
 
-input_str = input("{}\nPlease, enter your string: ".format(szerel["work1"]))
+input_str = input("{}\nPlease, enter your string: ".format(dictSzerel["work1"]))
 print(reverse_string(input_str))
 # Input: "the test drive a recursive function"
 # Output: "noitcnuf evisrucer a evird tset eht"
@@ -38,7 +38,7 @@ def check_str_palindrome(string):
         return check_str_palindrome(string[1:-1])
     return False
 
-input_str = input("{}\nPlease, enter your string: ".format(szerel["work2"]))
+input_str = input("{}\nPlease, enter your string: ".format(dictSzerel["work2"]))
 rep = ",.:;!?-#@$%^&*()_+|/'’"
 input_str = ''.join([x for x in input_str if x not in rep])
 
@@ -67,7 +67,7 @@ def count_occurrences(user_string, user_char):
     else:
         return count_occurrences(user_string[1:], user_char)
 
-user_string = input("{}\nPlease, enter your string: ".format(szerel["work3"]))
+user_string = input("{}\nPlease, enter your string: ".format(dictSzerel["work3"]))
 user_char = input("Please enter your char: ")
 
 print("Result: ", count_occurrences(user_string, user_char))
@@ -90,7 +90,7 @@ def calculate_power(number, degree):
     if degree > 0:
         return number * calculate_power(number, degree-1)
 
-input_number = input("{}\nPlease, enter your number: ".format(szerel["work4"]))
+input_number = input("{}\nPlease, enter your number: ".format(dictSzerel["work4"]))
 input_degree = input("Ok, enter degree: ")
 
 print("Result: ", calculate_power(float(input_number), float(input_degree)))
@@ -99,19 +99,33 @@ print("Result: ", calculate_power(float(input_number), float(input_degree)))
 # Output: 0.03125
 
 
-def fibonacci_positive(n):
-    if n == 0:
+def fibonacci_positive(user_input):
+    """
+    fibonacci function for positive values
+    :param user_input: integer value
+    :return: find the Nth number in the Fibonacci sequence
+    """
+    if user_input == 0:
         return 0
-    if n in (1, 2):
+    if user_input in (1, 2):
         return 1
-    return fibonacci_positive(n - 1) + fibonacci_positive(n - 2)
+    return fibonacci_positive(user_input - 1) + fibonacci_positive(user_input - 2)
 
-def fibonacci_negative(n):
-    return fibonacci_positive(n) * (-1)**(n+1)
+def fibonacci_negative(user_input):
+    """
+    fibonacci function for negative values
+    :param user_input: integer value
+    :return: find the Nth number in the Fibonacci sequence
+    """
+    return fibonacci_positive(user_input) * (-1)**(user_input+1)
 
 def fibonacci_input_validation():
+    """
+    user-entered data validation function
+    :return: the result of a positive or negative function, depending on the conditions
+    """
     try:
-        user_input = int(input("Enter integer: "))
+        user_input = int(input("{}\nEnter integer: ".format(dictSzerel["work5"])))
         if user_input == 0:
             return print(fibonacci_positive(user_input))
         elif user_input >= 1:
@@ -122,4 +136,5 @@ def fibonacci_input_validation():
             return user_input
     except ValueError:
         print("Invalid input. Enter an integer.")
+
 fibonacci_input_validation()
