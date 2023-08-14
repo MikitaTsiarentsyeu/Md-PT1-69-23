dd07b718bd2acb8d7f92629dc0de72c259088ff7
szerel <artyom_sherel@mail.ru>
2023-08-14 22:27:27 +0300

    add exception Task7_5(RAM).py

--- Tasks/Sherel/Task7/Task7_5/Task7_5(RAM).py
+++ Tasks/Sherel/Task7/Task7_5/Task7_5(RAM).py
@@ -14,13 +14,16 @@ def add_in_dict(key, value):
         return my_dict
 
 while True:
-    a = input('Enter the first value: ')
-    b = input('Enter the second value: ')
-    value = int(a) + int(b)
-    key = '({}, {})'.format(a, b)
-    result = find_value_to_json_file(key, my_dict)
-    if result is not None:
-        print('The sum of arguments {} and {} is equal {} (Cache)'.format(a, b, result))
-    else:
-        add_in_dict(key, value)
-        print('The sum of arguments {} and {} is equal {} (Online)'.format(a, b, value))
+    try:
+        a = input('Enter the first value: ')
+        b = input('Enter the second value: ')
+        value = int(a) + int(b)
+        key = '({}, {})'.format(a, b)
+        result = find_value_to_json_file(key, my_dict)
+        if result is not None:
+            print('The sum of arguments {} and {} is equal {} (Cache)'.format(a, b, result))
+        else:
+            add_in_dict(key, value)
+            print('The sum of arguments {} and {} is equal {} (Online)'.format(a, b, value))
+    except ValueError:
+        print('Enter integer value for argument "a" and "b"')
