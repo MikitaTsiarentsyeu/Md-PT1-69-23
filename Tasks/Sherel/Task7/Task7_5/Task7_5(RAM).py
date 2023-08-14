70ce391356774292de51a941a910ba0f444d77fb
szerel <artyom_sherel@mail.ru>
2023-08-14 22:22:27 +0300

    add Task7_5(RAM).py

---
+++ Tasks/Sherel/Task7/Task7_5/Task7_5(RAM).py
@@ -0,0 +1,26 @@
+my_dict = dict()
+
+def find_value_to_json_file(key, my_dict):
+    if key in my_dict:
+        return my_dict[key]
+    for value in my_dict.values():
+        if isinstance(value, dict):
+            result = find_value_to_json_file(key, value)
+            if result:
+                return result
+
+def add_in_dict(key, value):
+        my_dict[key] = value
+        return my_dict
+
+while True:
+    a = input('Enter the first value: ')
+    b = input('Enter the second value: ')
+    value = int(a) + int(b)
+    key = '({}, {})'.format(a, b)
+    result = find_value_to_json_file(key, my_dict)
+    if result is not None:
+        print('The sum of arguments {} and {} is equal {} (Cache)'.format(a, b, result))
+    else:
+        add_in_dict(key, value)
+        print('The sum of arguments {} and {} is equal {} (Online)'.format(a, b, value))
