c32e2e83f6b2b001af0a09394fa9acbe7a9f7211
szerel <artyom_sherel@mail.ru>
2023-08-14 22:34:34 +0300

    rename func, add instructions

--- Tasks/Sherel/Task7/Task7_5/Task7_5(RAM).py
+++ Tasks/Sherel/Task7/Task7_5/Task7_5(RAM).py
@@ -1,17 +1,27 @@
 my_dict = dict()
 
-def find_value_to_json_file(key, my_dict):
+def find_value_to_my_dict(key, my_dict):
+    """
+    Recursive function cache key lookup
+    :return: key/value or None
+    """
     if key in my_dict:
         return my_dict[key]
     for value in my_dict.values():
         if isinstance(value, dict):
-            result = find_value_to_json_file(key, value)
+            result = find_value_to_my_dict(key, value)
             if result:
                 return result
 
 def add_in_dict(key, value):
-        my_dict[key] = value
-        return my_dict
+    """
+    Function to group required parameters before writing them to my dict
+    :param key: '(a, b)'
+    :param value: summ a and b
+    :return: updated dictionary with new key and value
+    """
+    my_dict[key] = value
+    return my_dict
 
 while True:
     try:
@@ -19,7 +29,7 @@ while True:
         b = input('Enter the second value: ')
         value = int(a) + int(b)
         key = '({}, {})'.format(a, b)
-        result = find_value_to_json_file(key, my_dict)
+        result = find_value_to_my_dict(key, my_dict)
         if result is not None:
             print('The sum of arguments {} and {} is equal {} (Cache)'.format(a, b, result))
         else:
