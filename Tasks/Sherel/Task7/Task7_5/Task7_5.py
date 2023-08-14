1b6da32eac9643e445af2577ce8943b819d8d357
szerel <artyom_sherel@mail.ru>
2023-08-14 20:57:48 +0300

    optimized code, added light documentation

--- Tasks/Sherel/Task7/Task7_5/Task7_5.py
+++ Tasks/Sherel/Task7/Task7_5/Task7_5.py
@@ -1,42 +1,67 @@
 import json
 
-def open_file_for_read(file_path, metod):
-    with open(file_path, metod) as file:
+def open_file_for_read(file_path):
+    """
+    Function to open json file (for reuse without cloning)
+    """
+    with open(file_path, 'r') as file:
         data = json.load(file)
-        return data
+    return data
 
-def find_value_to_json_file(key, file_path):
-    data = open_file_for_read(file_path, metod="r")
-    def find(d, key):
-        if key in d:
-            return d[key]
-        for k, v in d.items():
-            if isinstance(v, dict):
-                result = find(v, key)
-                if result:
-                    return result
-    return find(data, key)
+def find_value_to_json_file(key, data):
+    """
+    Recursive function cache key lookup
+    :return: key/value or None
+    """
+    if key in data:
+        return data[key]
+    for value in data.values():
+        if isinstance(value, dict):
+            result = find_value_to_json_file(key, value)
+            if result:
+                return result
 
-def add_key_value_to_json_file(file_path, key, key_change, value):
-    data = open_file_for_read(file_path, metod="r")
+
+def add_key_value_to_json_file(data, key, key_change, value):
+    """
+    Function to group required parameters before writing them to cache
+    :param data: is open file for read
+    :param key: '(a, b)'
+    :param key_change: '(b, a)'
+    :param value: summ a and b
+    """
     data[key] = value
     data[key_change] = value
-    with open(file_path, 'w') as file:
-        json.dump(data, file, sort_keys=True, indent=4)
 
-while True:
-    try:
-        file_path = 'Task7_5-logs.json'
-        a = input('Enter the first value: ')
-        b = input('Enter the second value: ')
-        value = int(a) + int(b)
-        key = '({}, {})'.format(a, b)
-        key_change = '({}, {})'.format(b, a)
-        result = find_value_to_json_file(key, file_path)
-        if result != None:
-            print('The sum of arguments {} and {} is equal {} (CASH)'.format(a, b, result))
-        elif result is None:
-            add_key_value_to_json_file(file_path, key, key_change, value)
-            print('The sum of arguments {} and {} is equal {} (Online)'.format(a, b, value))
-    except:
-        print('Enter integer value for argument "a" and "b"')
+
+def main():
+    """
+    Sum function of two integers
+    :return: the result of the calculation - made in real time (if not in the cache) or will return the value from
+    the cache and will not perform the calculation
+    """
+    while True:
+        try:
+            file_path = 'Task7_5-logs.json'
+            a = input('Enter the first value: ')
+            b = input('Enter the second value: ')
+            value = int(a) + int(b)
+            key = '({}, {})'.format(a, b)
+            key_change = '({}, {})'.format(b, a)
+
+            data = open_file_for_read(file_path)
+
+            result = find_value_to_json_file(key, data)
+            if result is not None:
+                print('The sum of arguments {} and {} is equal {} (CASH)'.format(a, b, result))
+            else:
+                add_key_value_to_json_file(data, key, key_change, value)
+                with open(file_path, 'w') as file:
+                    json.dump(data, file, sort_keys=True, indent=4)
+                print('The sum of arguments {} and {} is equal {} (Online)'.format(a, b, value))
+        except ValueError:
+            print('Enter integer value for argument "a" and "b"')
+
+
+if __name__ == '__main__':
+    main()
