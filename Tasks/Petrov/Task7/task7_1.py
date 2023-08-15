8661849ae65fc8fcc7059638201b43cba1057651
KortezR <wowikus99@mail.ru>
2023-08-15 14:28:59 +0300

    Edit task7_1 and task7_2
    Changed logic for better task concept showing

--- Tasks/Petrov/Task7/task7_1.py
+++ Tasks/Petrov/Task7/task7_1.py
@@ -1,20 +1,19 @@
 from decimal import Decimal, InvalidOperation
-from functools import reduce
 
 
 def divide(num1, num2):
-    return num1 / num2
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
 
 
 while True:
     try:
         nums = [Decimal(i) for i in input("Input 2 numbers by space: ").split()]
-        print(f"Dividing {reduce(lambda x, y: f'{x} by {y}', nums)} equals {divide(*nums)}")
-        # equals to line below:
-        # print(f"Dividing {' by '.join(str(i) for i in nums)} equals {divide(*nums)}")
+        print("Processing of dividing elements")
+        print(f"Result is: {divide(*nums)}")
         break
-    except ZeroDivisionError:
-        print("Cannot divide by zero")
     except InvalidOperation:
         print("Invalid operation")
     except TypeError:
