82476280cf4ece80c0ef1565b798eb478fc74480
szerel <artyom_sherel@mail.ru>
2023-08-17 16:14:39 +0300

    added explanations and comments in 7.5 ram+decorator

--- Tasks/Sherel/Task7/Task7_5/Task7_5(ram+decorator)/Task7_5(decorator+ram).py
+++ Tasks/Sherel/Task7/Task7_5/Task7_5(ram+decorator)/Task7_5(decorator+ram).py
@@ -1,10 +1,19 @@
-"""this is the simplest example that I could squeeze out of my finger to create a decorator,
-the cache is crunched throughout the program, as soon as the program stops the dict is emptied"""
+print("""This program performs the addition of 2 value and has a decorator function that caches the return value of the
+function with a dictionary. If the function is called again with the same arguments, 
+it returns the cached value rather than recalculating it.""")
 
-my_dict = dict()
+my_dict = dict()  # cache - dictionary
 
 def cache(func):
+    """
+    A decorator function that caches the return value of a function with a dictionary.
+    :param func: def summ(x, y)
+    """
     def wrapper(*args, **kwargs):
+        """
+        :param args: first and second input value
+        :return: the result of the sum function,if the calculation has already been made - returns the result from cache
+        """
         key = '{}'.format(args)
         if key in my_dict:
             return print('Cache', my_dict[key])
@@ -12,15 +21,19 @@ def cache(func):
             result = func(*args, **kwargs)
             my_dict[key] = result
             return print('Online:', my_dict[key])
-        return result
     return wrapper
 
 @cache
 def summ(x, y):
+    """
+    Func summ values input first and second
+    :param x: first input
+    :param y: second input
+    """
     return x + y
 
 while True:
     try:
-        summ(int(input('a: ')), int(input('b: ')))
+        summ(int(input('Enter your first integer value: ')), int(input('Enter your second integer value: ')))
     except:
-        print('oops')
+        print('Unknown error or program terminated.')
