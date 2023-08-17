33adf8023c02ef092e6ee7740035c6426abbd335
KortezR <wowikus99@mail.ru>
2023-08-17 19:52:03 +0300

    Update task7_5_json_cache.py

--- Tasks/Petrov/Task7/task7_5_json_cache.py
+++ Tasks/Petrov/Task7/task7_5_json_cache.py
@@ -1,24 +1,30 @@
 from decimal import Decimal, InvalidOperation
 import json
 
-try:
-    file = open("cache.json", "r", encoding="UTF-8")
-except FileNotFoundError:
-    file = open("cache.json", "w+", encoding="UTF-8")
-    json.dump({}, file)
-    file.seek(0)
-cache = json.load(file)
-file.close
+caches = {}
 
 
 def caching(func):
+
+    try:
+        current_cache = f"{func.__name__}_cache.json"
+        file = open(current_cache, "r", encoding="UTF-8")
+    except FileNotFoundError:
+        file = open(current_cache, "w+", encoding="UTF-8")
+        json.dump({}, file)
+        file.seek(0)
+
+    caches[func.__name__] = json.load(file)
+    current_cache = caches[func.__name__]
+    file.close
+
     def wrapper(list):
         arguments = str(sorted(list))
-        if arguments in cache:
+        if arguments in current_cache:
             print("Result was taken from cache")
         else:
-            cache[arguments] = str(func(list))
-        return cache[arguments]
+            current_cache[arguments] = str(func(list))
+        return current_cache[arguments]
     return wrapper
 
 
@@ -38,7 +44,9 @@ while True:
     else:
         break
 
-
-file = open("cache.json", "w", encoding="UTF-8")
-json.dump(cache, file, indent=4)
-file.close
+if caches:
+    for key, value in caches.items():
+        file_name = f"{key}_cache.json"
+        file = open(file_name, "w", encoding="UTF-8")
+        json.dump(value, file, indent=4)
+        file.close
