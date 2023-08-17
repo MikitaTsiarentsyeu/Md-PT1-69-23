f601dee129b7b04cd591ca256b4a028ddea1be79
KortezR <wowikus99@mail.ru>
2023-08-17 14:17:06 +0300

    Document task7_4

---
+++ Tasks/Petrov/Task7/hm.py
@@ -0,0 +1,6 @@
+def positive_args_sum(*args) -> int:
+    """Return sum of all positive numbers from the given"""
+    return sum(filter(lambda x: x > 0, args))
+
+
+print(function)
--- Tasks/Petrov/Task7/task7_4.py
+++ Tasks/Petrov/Task7/task7_4.py
@@ -1,11 +1,15 @@
 import time
 
 
-def logs(func):
+def logs(func: object.function) -> object.function:
+    """Decorator function that logs the execution time, arguments and return value of a function to a file"""
     def wrapper(*args):
+
+        # Counting the execution time of function
         start_time = time.perf_counter()
         result = func(*args)
         execution_time = time.perf_counter() - start_time
+
         with open("logs7_4.txt", "w", encoding="UTF-8") as file:
             file.write(f"{func.__name__} was called\nArguments of this function: {args}\nExecution time: {execution_time:.8f} sec\nResult: {result}")
         return result
@@ -13,7 +17,8 @@ def logs(func):
 
 
 @logs
-def positive_args_sum(*args):
+def positive_args_sum(*args) -> int:
+    """Return sum of all positive numbers from the given"""
     return sum(filter(lambda x: x > 0, args))
 
 
