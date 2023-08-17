b4df6084b54e9a8623e85637666d119eb98cdc5a
KortezR <wowikus99@mail.ru>
2023-08-17 11:18:04 +0300

    Document task7_1

--- Tasks/Petrov/Task7/task7_1.py
+++ Tasks/Petrov/Task7/task7_1.py
@@ -1,7 +1,8 @@
 from decimal import Decimal, InvalidOperation
 
 
-def divide(num1, num2):
+def divide(num1: Decimal, num2: Decimal) -> Decimal:
+    """Return the result of division of two numbers"""
     try:
         return num1 / num2
     except ZeroDivisionError:
@@ -10,11 +11,21 @@ def divide(num1, num2):
 
 while True:
     try:
-        nums = [Decimal(i) for i in input("Input 2 numbers by space: ").split()]
+        print("Input 2 numbers by space:", end=" ")
+
+        # Creating list of numbers to calculate their division
+        nums = [Decimal(i) for i in input().split()]
+
         print("Processing of dividing elements")
         print(f"Result is: {divide(*nums)}")
         break
     except InvalidOperation:
+
+        # If input can't be converted to Decimal, InvalidOperation raises
+        # It also raises for [0, 0] and [inf, inf] cases
         print("Invalid operation")
+
     except TypeError:
+
+        # If the list has more than 2 numbers, TypeError raises
         print("You need only 2 numbers")
