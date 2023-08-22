6119e5045c6ead01d5cb54d885376eebfa456d17
KortezR <wowikus99@mail.ru>
2023-08-22 13:39:18 +0300

    Implement adding feature

--- Tasks/Petrov/Task8/back_end.py
+++ Tasks/Petrov/Task8/back_end.py
@@ -5,6 +5,7 @@ import time
 with open("anime_series.json", "r", encoding="UTF-8") as file:
     data = json.load(file)
 
+fields = ["title", "studios", "year", "genres"]
 
 def separation():
     print("-"*30)
@@ -48,8 +49,40 @@ def format_anime_series(anime_series):
             print(f"{key}: {value}")
 
 
+def filling_new_series(new_title):
+    while True:
+        new_series = [new_title]
+        print("Fill the next parameters of this series:")
+        try:
+            for field in fields[1:]:
+                value = input(f"{field} - ")
+                if field == "year":
+                    new_series.append(int(value))
+                elif field == "genres":
+                    new_series.append(value.split(", "))
+                else:
+                    new_series.append(value)
+            return {field: value for field, value in zip(fields, new_series)}
+        except ValueError:
+            print("Wrong info for this field, error happened, try again")
+
+
 def add_new_anime_series():
-    pass
+    new_title = input("Which anime series do you want to add? ")
+
+    # to do - search_by_title(new_title)
+    current_titles = [series["title"] for series in data.values()]
+    if new_title in current_titles:
+        print("This series is already in our storage")
+    else:
+        print("Okay")
+
+        new_series = filling_new_series(new_title)
+        new_pos = int(list(data.keys())[-1])+1
+        data[str(new_pos)] = new_series
+        with open("anime_series.json", "w", encoding="UTF-8") as file:
+            json.dump(data, file, indent=4)
+        print("New anime series was added to storage")
 
 
 def search_anime_series():
