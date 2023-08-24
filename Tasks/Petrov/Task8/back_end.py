b8dbf131ad4323647db27215f85d1f697ac57fde
KortezR <wowikus99@mail.ru>
2023-08-25 00:49:19 +0300

    Edit logic and document

--- Tasks/Petrov/Task8/back_end.py
+++ Tasks/Petrov/Task8/back_end.py
@@ -1,7 +1,11 @@
+"""Module implements operation side for digital anime series storage"""
+
 import json
 
+FILE_PATH = "anime_series.json"
 
-with open("anime_series.json", "r", encoding="UTF-8") as file:
+# Loading current storage database
+with open(FILE_PATH, "r", encoding="UTF-8") as file:
     data = json.load(file)
 
 fields = ["Title", "Studios", "Year", "Genres"]
@@ -16,23 +20,35 @@ def goodbye_message():
 
 
 def action():
+    """Returns user input as option choice"""
+
     return input("Your choice: ")
 
 
 def all_anime_series():
+    """Returns list of strings of all anime series in storage"""
+
     result = []
     for pos, series in data.items():
+
+        # Creating blank string for each element
         element = ""
         element = f"{element}{pos}. "
         lines = '\n'.join(line for line in format_anime_series(series))
         element = f"{element}{lines}"
+
+        # Adding string interpretation of anime series in list
         result.append(element)
     return result
 
 
 def format_anime_series(anime_series):
+    """Returns list of strings with fields of one anime series"""
+
     result = []
     for key, value in anime_series.items():
+
+        # Creating blank string for each field
         line = ""
         if key == "Title":
             line = f"{line}{value}"
@@ -40,11 +56,19 @@ def format_anime_series(anime_series):
             line = f"{line}{key}: {', '.join(genre for genre in value)}"
         else:
             line = f"{line}{key}: {value}"
+
+        # Adding string interpretation of field in list
         result.append(line)
     return result
 
 
 def filling_new_series(new_title):
+    """Returns dictionary object of new anime series
+
+    new_title
+        string for title of new anime series"""
+
+    # Creating a list of all fields for new anime series
     new_series = [new_title]
     for field in fields[1:]:
         value = input(f"{field} - ")
@@ -58,31 +82,78 @@ def filling_new_series(new_title):
 
 
 def add_new_anime_series(new_series):
+    """Adding new anime series to storage
+
+    new_series
+        new anime series as dictionary"""
+
     new_pos = int(list(data.keys())[-1])+1
+
+    # Adding new anime series to local data dictionary
     data[str(new_pos)] = new_series
-    with open("anime_series.json", "w", encoding="UTF-8") as file:
+
+    with open(FILE_PATH, "w", encoding="UTF-8") as file:
         json.dump(data, file, indent=4)
 
 
+def search_anime_series_by_field(field_seek, input_text):
+    """Searching for anime series by field from inputted text.
+    Returns generator with proper anime series
+
+    field_seek
+        string interpretation of searching field
+    input_text
+        inputted text as search line"""
+
+    if field_seek == "Title":
+        return search_by_title(input_text)
+    if field_seek == "Studios":
+        return search_by_studios(input_text)
+    if field_seek == "Year":
+        return search_by_year(int(input_text))
+    if field_seek == "Genres":
+        return search_by_genres(input_text.split(", "))
+
+
 def search_by_title(title_seek):
+    """Returns a generator that has anime series with proper title
+
+    title_seek
+        string line as search for title"""
+
     for anime_series in data.values():
         if anime_series["Title"].lower() == title_seek.lower():
             yield anime_series
 
 
 def search_by_studios(studios_seek):
+    """Returns a generator that has anime series with proper studios
+
+    studios_seek
+        string line as search for studios"""
+
     for anime_series in data.values():
         if anime_series["Ttudios"].lower() == studios_seek.lower():
             yield anime_series
 
 
 def search_by_year(year_seek):
+    """Returns a generator that has anime series with proper year
+
+    year_seek
+        integer as search for year"""
+
     for anime_series in data.values():
         if anime_series["Year"] == year_seek:
             yield anime_series
 
 
 def search_by_genres(genres_seek):
+    """Returns a generator that has anime series with proper genres
+
+    genres_seek
+        list of strings as search for genres"""
+
     for anime_series in data.values():
         set_seek = set([genre.lower() for genre in genres_seek])
         set_current = set([genre.lower() for genre in anime_series["Genres"]])
--- Tasks/Petrov/Task8/front_end.py
+++ Tasks/Petrov/Task8/front_end.py
@@ -1,24 +1,35 @@
+"""Module implements console UI for digital anime series storage"""
+
 import time
 import back_end
 
 
 def separation():
+    """Printing the line and waiting for visual part"""
+
     print("-"*30)
     time.sleep(1.5)
 
 
 def welcome():
+    """Printing the welcome message"""
+
     print(back_end.greeting_message())
     separation()
 
 
 def ending():
+    """Printing the goodbye message"""
+
     separation()
     print(back_end.goodbye_message())
 
 
 def process():
+    """Main UI function to manage the usage proccess"""
+
     while True:
+
         print("""What would you like to do?
 
         1. List all current anime series
@@ -26,8 +37,11 @@ def process():
         3. Search an anime series
 
         If you want to quit the program, just press Enter""", end="\n\n")
+
         action = back_end.action()
         print()
+
+        # Processing the function based on selected option
         if action == "":
             break
         elif action == "1":
@@ -38,33 +52,48 @@ def process():
             search_anime_series()
         else:
             print("Uknown operation")
+
         separation()
 
 
 def list_all_anime_series():
+    """Printing all anime series in the storage"""
+
     print("Currently we have anime series such as:", end="\n\n")
     print('\n\n'.join(series for series in back_end.all_anime_series()))
 
 
 def add_new_anime_series():
+    """Describing the process of adding new anime series to the storage"""
+
     new_title = input("Which anime series do you want to add? ")
     try:
+
+        # If anime series with such title was found, nothing will be added
         next(back_end.search_by_title(new_title))
         print("This series is already in our storage")
     except StopIteration:
         print("Okay")
         try:
             while True:
+
+                # Filling other fields of the object
                 print("Fill the next parameters of this series:")
                 new_series = back_end.filling_new_series(new_title)
+
+                # Adding new object to storage
                 back_end.add_new_anime_series(new_series)
                 print("New anime series was added to storage")
                 break
         except ValueError:
+
+            # Error for putting not an int for year
             print("Wrong info for this field, error happened, try again")
 
 
 def search_anime_series():
+    """Describing the process of searching an anime series from storage"""
+
     while True:
         try:
             print("""Choose field to search anime series from:
@@ -76,29 +105,32 @@ def search_anime_series():
 
                 5. Cancel the search""", end="\n\n")
             search_field = int(back_end.action())
-            obj = back_end.fields[search_field-1]
-            print(f"\n{obj} you're searching for:", end=" ")
-            if search_field == 1:
-                res = back_end.search_by_title(input())
-            elif search_field == 2:
-                res = back_end.search_by_studios(input())
-            elif search_field == 3:
-                res = back_end.search_by_year(int(input()))
-            elif search_field == 4:
-                res = back_end.search_by_genres(input().split(", "))
-            elif search_field == 5:
+            if search_field == 5:
                 break
-            else:
-                raise ValueError
+            field = back_end.fields[search_field-1]
+
+            # Processing the search function based on selected search field
+            print(f"\n{field} you're searching for:", end=" ")
+            res = back_end.search_anime_series_by_field(field, input())
+
             print("\nThe following anime series were found: ")
+
+            # Showing the search results to user
             order = 1
             for item in res:
                 print(f"\n{order}.", end=" ")
                 anime = back_end.format_anime_series(item)
                 print('\n'.join(line for line in anime))
                 order += 1
+
+            # Printing the result if nothing was found (kinda)
             if order == 1:
                 print("No search results")
             break
-        except ValueError:
+        except (ValueError, IndexError):
+
+            # If something wrong was inputted
+            # as option choice, ValueError raises
+            # It also raises if the year was input incorrectly
+            # If non-existing option was picked, IndexError raises
             print("Wrong input")
